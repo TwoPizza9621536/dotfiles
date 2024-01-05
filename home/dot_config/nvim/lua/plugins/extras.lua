29e5e832b6c7eace9eb19382724f08790dbaf3fa
Samuel Wu <twopizza9621536@gmail.com>
2024-01-04 20:52:26 -0500

    feat!: Remove unnessary configurations

--- home/dot_config/nvim/lua/plugins/extras.lua
+++
@@ -1,6 +0,0 @@
-return {
-  { "alanfortlink/blackjack.nvim", cmd = "BlackJackNewGame" },
-  { "eandrju/cellular-automaton.nvim", cmd = "CellularAutomaton " },
-  { "letieu/hacker.nvim", cmd = { "Hack", "HackFollow" } },
-  { "seandewar/nvimesweeper", cmd = "Nvimesweeper" },
-}
