3083a5952589d5046efc03bb6d49c21a0a904930
Samuel Wu <twopizza9621536@gmail.com>
2024-01-04 21:22:12 -0500

    fix: Add corepack aliases

--- home/dot_config/zsh/rc.d/zz_alias.zsh
+++ home/dot_config/zsh/rc.d/zz_alias.zsh
@@ -11,6 +11,12 @@ alias svn="svn --config-dir $XDG_CONFIG_HOME/subversion"
 alias icat="kitty +kitten icat"
 alias tb="toolbox enter"
 
+# Corepack
+alias yarn="corepack yarn"
+alias yarnpkg="corepack yarnpkg"
+alias pnpm="corepack pnpm"
+alias pnpx="corepack pnpx"
+
 # Load more specific 'run-help' function from $fpath
 (( $+aliases[run-help] )) && unalias run-help && autoload -Uz run-help
 alias help=run-help
