2d47009d7b7b09cc69b378c70046818b7e1cb367
Samuel Wu <twopizza9621536@gmail.com>
2024-08-06 16:50:37 -0400

    Add NeoVim config again
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

---
+++ home/dot_config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "Mofiqul/vscode.nvim",
+    opts = {
+      transparent = true,
+      italic_comments = true,
+      underline_links = true,
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vscode",
+    },
+  },
+
+  { "folke/tokyonight.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+}
